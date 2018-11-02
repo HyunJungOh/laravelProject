c7f4bbe107eea4d0497f7cd1fbbdbb5e6ca110aa
HyunJungOh <hjoh@selva-i.co.jp>
2018-11-03 08:23:41 +0900

    first routing and moved styles to styles.css

--- todoList/routes/web.php
+++ todoList/routes/web.php
@@ -15,6 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todoList', function () {
-    return view('todoList');
-});
+Route::get('/todoList', 'ListController@getTodoList');
+
